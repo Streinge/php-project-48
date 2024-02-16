2ca16f18ab87969991dd1ce4e643ed8fc11426b0
OlegSofonov <olesof@mail.ru>
2024-02-16 23:35:37 +0500

    update .gitinore & Parser.php add RecursiveTest.php

--- src/Parsers.php
+++ src/Parsers.php
@@ -7,6 +7,7 @@ use Symfony\Component\Yaml\Yaml;
 function jsonInArray(string $filepath): array|null
 {
     $dataFile1 = readFile($filepath);
+    print_r($dataFile1);
     $jsonArray1 = json_decode($dataFile1, true) ?? [];
 
     return (!is_null($dataFile1)) ? $jsonArray1 : null;
@@ -18,3 +19,5 @@ function yamlInArray(string $filepath): array|null
 
     return (!is_null($dataFile1)) ? Yaml::parse($dataFile1) : null;
 }
+
+print_r(jsonInArray('/home/streinge/php-project-48/tests/fixtures/file5.json'));
