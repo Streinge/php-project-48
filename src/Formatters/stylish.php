909e6b6a49a5cbcd6d68bef860895e427cf48e4f
OlegSofonov <olesof@mail.ru>
2024-02-25 12:13:16 +0500

    update stylish.php 3

--- src/Formatters/stylish.php
+++ src/Formatters/stylish.php
@@ -7,7 +7,7 @@ function toString(mixed $value): string
     return is_null($value) ? 'null' : trim(var_export($value, true), "'");
 }
 
-function stylish($value, string $replacer = ' ', int $spacesCount = 1): string
+function stylish($value, string $replacer = ' ', int $spacesCount = 2): string
 {
 
     $iter = function ($currentValue, $depth) use (&$iter, $replacer, $spacesCount) {
